8bb470945dc763df8d879f059d87341a5970ce36
Illia Chernyk <ilyachernykan@gmail.com>
2024-10-21 23:53:18 +0300

    Fix phone

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 1. Fix warnings in console
-2. Fix squadhelp logo in Header and Payment 
+2. Fix squadhelp logo in Header and Payment
+3. Fix phone number : create constant and use it
--- client/src/constants.js
+++ client/src/constants.js
@@ -2,6 +2,7 @@ const env = process.env.NODE_ENV || 'development';
 const serverIP = 'localhost';
 const serverPort = 3000;
 const CONSTANTS = {
+  CONTACT_PHONE: '(877) 355-3585',
   CUSTOMER: 'customer',
   CREATOR: 'creator',
   CONTEST_STATUS_ACTIVE: 'active',
