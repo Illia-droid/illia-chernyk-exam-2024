9fe918f8021b62cd2c8943eb5c61f1d91e58ba50
Illia Chernyk <ilyachernykan@gmail.com>
2025-02-13 21:06:21 +0200

    Fix warning

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -4,7 +4,7 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  const root = ReactDOM.createRoot(document.getElementById('root'));
+  const root = ReactDOM.createRoot(div); 
   root.render(<App />);
-  ReactDOM.unmountComponentAtNode(div);
+  root.unmount(); 
 });
